f38abc516b2a88ed94615328ea905435a61c3891
Philip Müller <philm@manjaro.org>
2021-05-27 09:43:36 +0200

    [cutefish] use settings package

--- community/cutefish/desktop-overlay/etc/manjaro/manjaro-tweaks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Remove unwanted Desktop files
-rm -v /usr/share/applications/avahi-discover.desktop
-rm -v /usr/share/applications/bssh.desktop
-rm -v /usr/share/applications/bvnc.desktop
-rm -v /usr/share/applications/qv4l2.desktop
-rm -v /usr/share/applications/qvidcap.desktop
-rm -v /usr/share/applications/UserFeedbackConsole.desktop
